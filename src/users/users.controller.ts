90dd38a607d8fb0585f136921c01a4ef366183fc
tamlongnguyen <longnt@i.lisod.vn>
2023-07-04 17:02:34 +0700

    update controller service module users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, ParseIntPipe, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  public async getUserList(
+    @Query('page', ParseIntPipe) page: number = 1,
+    @Query('limit', ParseIntPipe) limit: number = 10,
+  ) {
+    const users = await this.usersService.getList(page, limit);
+    return {
+      page,
+      limit,
+      data: users,
+    };
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient, User } from '@prisma/client';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  public async getList(page: number, limit: number): Promise<User[]> {
+    try {
+      const offset = (page - 1) * limit;
+      const users = await this.prisma.user.findMany({
+        skip: offset,
+        take: limit,
+      });
+      return users;
+    } catch (error) {
+      console.log({ userListError: error });
+      return error;
+    }
+  }
+}
